1a540684462dd1a0b9dbb54be5f323d150bae9b8
Fl4son <dinarhertz@gmail.com>
2023-05-17 10:31:49 +0300

    Fix max combobox size

--- EducationalPartApp/Codes/Native.cs
+++ EducationalPartApp/Codes/Native.cs
@@ -21,7 +21,6 @@ namespace EducationalPartApp.Codes
         {
             MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
 
-            // Adjust the maximized size and position to fit the work area of the correct monitor
             int MONITOR_DEFAULTTONEAREST = 0x00000002;
             IntPtr monitor = Native.MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
 
@@ -43,25 +42,12 @@ namespace EducationalPartApp.Codes
             Marshal.StructureToPtr(mmi, lParam, true);
         }
 
-
-        /// <summary>
-        /// POINT aka POINTAPI
-        /// </summary>
         [StructLayout(LayoutKind.Sequential)]
         public struct POINT
         {
-            /// <summary>
-            /// x coordinate of point.
-            /// </summary>
             public int x;
-            /// <summary>
-            /// y coordinate of point.
-            /// </summary>
             public int y;
 
-            /// <summary>
-            /// Construct a point of coordinates (x,y).
-            /// </summary>
             public POINT(int x, int y)
             {
                 this.x = x;
@@ -79,57 +65,35 @@ namespace EducationalPartApp.Codes
             public POINT ptMaxTrackSize;
         };
 
-        /// <summary>
-        /// </summary>
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
         public class MONITORINFO
-        {
-            /// <summary>
-            /// </summary>            
+        {        
             public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
-
-            /// <summary>
-            /// </summary>            
-            public RECT rcMonitor = new RECT();
-
-            /// <summary>
-            /// </summary>            
-            public RECT rcWork = new RECT();
-
-            /// <summary>
-            /// </summary>            
+            public RECT rcMonitor = new RECT();        
+            public RECT rcWork = new RECT();           
             public int dwFlags = 0;
         }
 
 
-        /// <summary> Win32 </summary>
         [StructLayout(LayoutKind.Sequential, Pack = 0)]
         public struct RECT
         {
-            /// <summary> Win32 </summary>
             public int left;
-            /// <summary> Win32 </summary>
             public int top;
-            /// <summary> Win32 </summary>
             public int right;
-            /// <summary> Win32 </summary>
             public int bottom;
 
-            /// <summary> Win32 </summary>
             public static readonly RECT Empty = new RECT();
 
-            /// <summary> Win32 </summary>
             public int Width
             {
-                get { return Math.Abs(right - left); }  // Abs needed for BIDI OS
+                get { return Math.Abs(right - left); } 
             }
-            /// <summary> Win32 </summary>
             public int Height
             {
                 get { return bottom - top; }
             }
 
-            /// <summary> Win32 </summary>
             public RECT(int left, int top, int right, int bottom)
             {
                 this.left = left;
@@ -138,8 +102,6 @@ namespace EducationalPartApp.Codes
                 this.bottom = bottom;
             }
 
-
-            /// <summary> Win32 </summary>
             public RECT(RECT rcSrc)
             {
                 this.left = rcSrc.left;
@@ -148,43 +110,34 @@ namespace EducationalPartApp.Codes
                 this.bottom = rcSrc.bottom;
             }
 
-            /// <summary> Win32 </summary>
             public bool IsEmpty
             {
                 get
                 {
-                    // BUGBUG : On Bidi OS (hebrew arabic) left > right
                     return left >= right || top >= bottom;
                 }
             }
-            /// <summary> Return a user friendly representation of this struct </summary>
             public override string ToString()
             {
                 if (this == RECT.Empty) { return "RECT {Empty}"; }
                 return "RECT { left : " + left + " / top : " + top + " / right : " + right + " / bottom : " + bottom + " }";
             }
 
-            /// <summary> Determine if 2 RECT are equal (deep compare) </summary>
             public override bool Equals(object obj)
             {
                 if (!(obj is Rect)) { return false; }
                 return (this == (RECT)obj);
             }
 
-            /// <summary>Return the HashCode for this struct (not garanteed to be unique)</summary>
             public override int GetHashCode()
             {
                 return left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
             }
-
-
-            /// <summary> Determine if 2 RECT are equal (deep compare)</summary>
             public static bool operator ==(RECT rect1, RECT rect2)
             {
                 return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom);
             }
 
-            /// <summary> Determine if 2 RECT are different(deep compare)</summary>
             public static bool operator !=(RECT rect1, RECT rect2)
             {
                 return !(rect1 == rect2);
